bb4d2ff048d842154cbde1a86304a5d7b279c8f5
K Hudson <dev@lotech.info>
2020-08-02 10:11:49 -0700

    Add more scenarios with pending tests.

--- src/test/java/LenderSteps.java
+++ src/test/java/LenderSteps.java
@@ -30,4 +30,84 @@ public class LenderSteps {
     public void i_should_be_told(String expectedAnswer) {
         assertEquals(expectedAnswer, actualAnswer);
     }
+
+    @Given("I am a lender")
+    public void i_am_a_lender() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I check my available funds")
+    public void i_check_my_available_funds() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("I should see the current balance")
+    public void should_see_current_balance() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("a customer requests a loan")
+    public void a_customer_requests_a_loan() {
+        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+    }
+    @When("the amount is larger than my available balance")
+    public void the_amount_is_larger_than_my_available_balance() {
+        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+    }
+    @Then("they will be denied")
+    public void they_will_be_denied() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the loan {string}, {string}, and number of {string}")
+    public void the_loan_and_number_of(String principal, String interest, String payments) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("a customer has {string} credit score, DTI of {string} and deposits {string} from savings")
+    public void a_customer_has_credit_score_dti_of_and_deposits_from_savings(String creditScore, String dti, String deposit) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("I have sufficient funds to make a loan")
+    public void i_have_sufficient_funds_to_make_a_loan() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("I sent a loan offer")
+    public void i_sent_a_loan_offer() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("an current balance of {string} in my account")
+    public void an_current_balance_of_in_my_account(String startBalance) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("I have a good loan candidate")
+    public void i_have_a_good_loan_candidate() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("a good candidate")
+    public void a_good_candidate() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("a loan contract was sent to a customer")
+    public void a_loan_contract_was_sent_to_a_customer() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
--- src/test/resources/mortgage_lender/lender.feature
+++ src/test/resources/mortgage_lender/lender.feature
@@ -24,3 +24,50 @@ Feature: Lender Management
     Examples:
       | principal | interest  | payments | result |
       | 100000    | 0.12      | 300      | 250    |
+
+    Scenario Outline: Lender appraises a customers credit worthiness
+      Given a customer has "<credit score>" credit score, DTI of "<dti>" and deposits "<deposit>" from savings
+      When they apply for a loan
+      Then they are qualified for it
+
+      Examples:
+        | credit score |   dti   |  deposit  |
+        | 620          |  25     | 10000     |
+
+    Scenario: Lender offers loan contract to a qualified customer
+      Given I have sufficient funds to make a loan
+      When I find a qualified applicant
+      Then I offer them a loan
+
+    Scenario: Lender receives a commission from approving a loan
+      Given I sent a loan offer
+      When a qualified applicant accepts
+      Then I approve the loan
+      And I receive a commission of 2%
+
+    Scenario Outline: Lender disburses loan funds to borrower
+      Given an current balance of "<start balance>" in my account
+      When I disburse a loan of "<loan>" to a borrower
+      Then my balance should be "<end balance>"
+      Examples:
+        | start balance |   loan   |  end balance  |
+        | 1000000       |  250000  |  750000       |
+
+    Scenario Outline: Lender sets expiration date on a loan offer
+      Given I have a good loan candidate
+      When I extend a loan offer on "<offer date>"
+      Then the offer will expire "<expires>"
+      Examples:
+        | offer date |   expires    |
+        | 06-01-2075 |  06-04-2075  |
+
+    Scenario: Lender manages available funds and pending funds to avoid bankruptcy
+      Given a good candidate
+      When I extend a loan offer
+      Then the loan amount is moved from available funds to pending funds
+
+    Scenario: Lender moves pending funds to available when offer expires
+      Given a loan contract was sent to a customer
+      When the contract expires without a response from the customer
+      Then the amount is moved from pending funds to available funds
+
